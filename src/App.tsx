4be008be26557c2dadc25fd47572ffb025427339
Denis Shapochkin <deniska.vla@gmail.com>
2021-12-26 12:29:04 +0200

    configuring WebPack

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const App = () => {
+  return <div>РАБОТАЕТ УСЕ !!!</div>;
+};
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["source-map-loader"],
+        enforce: "pre",
+      },
+      {
+        test: /\.js(x?)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpe?g|gif|jp2|webp)$/,
+        loader: "file-loader",
+        options: {
+          name: "images/[name].[ext]",
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new ESLintPlugin({}),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devServer: {
+    port: 3003,
+  },
+  devtool: "inline-source-map",
+};
